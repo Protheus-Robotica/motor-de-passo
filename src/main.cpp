daa415113afe9ab3884f91e4c2f7cb74196c2136
Gvinfinity <bielviniciussoares@gmail.com>
2022-09-16 02:16:36 -0300

    Substitui Interrupções OVF por OCM

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
-#define MOTOR_OUT_1 35
+#define MOTOR_OUT_1 11
 #define MOTOR_DIR_1 33
 #define MOTOR_EN_1 31
 
-#define MOTOR_OUT_2 37
+#define MOTOR_OUT_2 5
 #define MOTOR_DIR_2 39
 #define MOTOR_EN_2 41
 
-#define MOTOR_OUT_3 43
+#define MOTOR_OUT_3 6
 #define MOTOR_DIR_3 45
 #define MOTOR_EN_3 47
 
@@ -15,7 +15,7 @@
 class Stepper
 {
 private:
-    volatile uint16_t *TCNT;
+    volatile uint16_t *OCRA;
     volatile uint8_t *TCCRA, *TCCRB, *TIMSK;
 
 public:
@@ -24,14 +24,14 @@ public:
     int out_port, dir_port, en_port; 
     bool direction;
 
-    Stepper(volatile uint16_t *TCNT, volatile uint8_t *TCCRA, volatile uint8_t *TCCRB, volatile uint8_t *TIMSK, const int out_port, const int dir_port, const int en_port);
+    Stepper(volatile uint16_t *OCRA, volatile uint8_t *TCCRA, volatile uint8_t *TCCRB, volatile uint8_t *TIMSK, volatile uint8_t *TIFR, const int out_port, const int dir_port, const int en_port);
     void setSpeed(short speed);
     void setDirection(bool direction, int16_t steps);
 };
 
-Stepper::Stepper(volatile uint16_t *c_TCNT, volatile uint8_t *c_TCCRA, volatile uint8_t *c_TCCRB, volatile uint8_t *c_TIMSK, const int c_out_port, const int c_dir_port, const int c_en_port)
+Stepper::Stepper(volatile uint16_t *c_OCRA, volatile uint8_t *c_TCCRA, volatile uint8_t *c_TCCRB, volatile uint8_t *c_TIMSK, const int c_out_port, const int c_dir_port, const int c_en_port)
 {
-    TCNT = c_TCNT;
+    OCRA = c_OCRA;
     TCCRA = c_TCCRA;
     TCCRB = c_TCCRB;
     TIMSK = c_TIMSK;
@@ -39,10 +39,10 @@ Stepper::Stepper(volatile uint16_t *c_TCNT, volatile uint8_t *c_TCCRA, volatile 
     dir_port = c_dir_port;
     en_port = c_en_port;
 
-    *TCNT = this->timer_reset_v;
-    *TCCRA = 0b00000000;
-    *TCCRB = 0b00000010;
-    *TIMSK = 0b00000000;
+    *OCRA = this->timer_reset_v;
+    *TCCRA |= (1 << );
+    *TCCRB |= ((1 << WGM12) | (1 << CS11));
+    *TIMSK |= (1 << OCIE1A); // "| (1 << OCIE1B) | (1 << OCIE1C)" TO ENABLE OTHER MATCH COMPARE INTERUPTS
     direction = false;
     steps = -1;
     
@@ -57,17 +57,17 @@ void Stepper::setSpeed(short speed)
 {
     if (speed == 0)
     {
-        *TIMSK = 0b00000000;
+        *TCCRB &= ~(1 << CS11);
         digitalWrite(en_port, HIGH);
 
     } else
     {
-        *TIMSK = 0b00000001;
+        *TCCRB |= (1 << CS11);
         digitalWrite(en_port, LOW);
     }
     
     int frequency = 8*speed + 200;
-    timer_reset_v = 65536 - (1000000/frequency);
+    timer_reset_v = (1000000/frequency);
 };
 
 void Stepper::setDirection(bool direction, int16_t distance)
@@ -80,12 +80,10 @@ void Stepper::setDirection(bool direction, int16_t distance)
     steps = (distance == -1)?-1:(distance/1.419)*800;
 }
 
-//Stepper motor[] = {Stepper(&TCNT1, &TCCR1A, &TCCR1B, &TIMSK1, MOTOR_OUT_1, MOTOR_DIR_1), Stepper(&TCNT3, &TCCR3A, &TCCR3B, &TIMSK3, MOTOR_OUT_2, MOTOR_DIR_2), Stepper(&TCNT4, &TCCR4A, &TCCR4B, &TIMSK4, MOTOR_OUT_3, MOTOR_DIR_3)};
 Stepper *motor = (Stepper*) malloc(3 * sizeof(Stepper));
 
-ISR(TIMER1_OVF_vect)
+ISR(TIMER1_COMPA_vect)
 {
-    TCNT1 = motor[0].timer_reset_v;
     if (motor[0].steps == 0)
     {
         motor[0].setSpeed(0);
@@ -93,13 +91,10 @@ ISR(TIMER1_OVF_vect)
     {
         motor[0].steps--;
     }
-    
-    digitalWrite(motor[0].out_port, !digitalRead(motor[0].out_port));
 }
 
-ISR(TIMER3_OVF_vect)
+ISR(TIMER3_COMPA_vect)
 {
-    TCNT3 = motor[1].timer_reset_v;
     if (motor[1].steps == 0)
     {
         motor[1].setSpeed(0);
@@ -107,13 +102,10 @@ ISR(TIMER3_OVF_vect)
     {
         motor[1].steps--;
     }
-
-    digitalWrite(motor[1].out_port, !digitalRead(motor[1].out_port));
 }
 
 ISR(TIMER4_OVF_vect)
 {
-    TCNT4 = motor[2].timer_reset_v;
     if (motor[2].steps == 0)
     {
         motor[2].setSpeed(0);
@@ -121,16 +113,15 @@ ISR(TIMER4_OVF_vect)
     {
         motor[2].steps--;
     }
-
-    digitalWrite(motor[2].out_port, !digitalRead(motor[2].out_port));
-
 }
 
+
+ main()
 void setup() {
     Serial.begin(9600);
-    motor[0] = Stepper(&TCNT1, &TCCR1A, &TCCR1B, &TIMSK1, MOTOR_OUT_1, MOTOR_DIR_1, MOTOR_EN_1);
-    motor[1] = Stepper(&TCNT3, &TCCR3A, &TCCR3B, &TIMSK3, MOTOR_OUT_2, MOTOR_DIR_2, MOTOR_EN_2);
-    motor[2] = Stepper(&TCNT4, &TCCR4A, &TCCR4B, &TIMSK4, MOTOR_OUT_3, MOTOR_DIR_3, MOTOR_EN_3);
+    motor[0] = Stepper(&OCR1A, &TCCR1A, &TCCR1B, &TIMSK1, MOTOR_OUT_1, MOTOR_DIR_1, MOTOR_EN_1);
+    motor[1] = Stepper(&OCR3A, &TCCR3A, &TCCR3B, &TIMSK3, MOTOR_OUT_2, MOTOR_DIR_2, MOTOR_EN_2);
+    motor[2] = Stepper(&OCR4A, &TCCR4A, &TCCR4B, &TIMSK4, MOTOR_OUT_3, MOTOR_DIR_3, MOTOR_EN_3);
 }
 
 void loop() {
