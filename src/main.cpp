f409eeefbdacebef2a5bc25c834428693cee86d0
Gvinfinity <bielviniciussoares@gmail.com>
2023-10-31 22:10:00 -0300

    Fixed path (SNCT)

--- src/main.cpp
+++ src/main.cpp
@@ -44,10 +44,15 @@ class Stepper
         volatile uint16_t *OCR;
         int out_port, dir_port, en_port; 
         bool direction;
+        int distance;
+        bool moving;
+        int totalsteps;
 
         Stepper(volatile uint16_t *OCR, volatile uint8_t *TCCRA, volatile uint8_t *TCCRB, volatile uint8_t *TIMSK, const uint8_t OCIE, const uint8_t COM0, const int out_port, const int dir_port, const int en_port);
         void setSpeed(short speed);
         void setDirection(bool direction, int16_t steps);
+        void setFrequency(uint16_t frequency);
+        void setSteps(bool direction, int c_steps);
 };
 
 Stepper::Stepper(volatile uint16_t *c_OCR, volatile uint8_t *c_TCCRA, volatile uint8_t *c_TCCRB, volatile uint8_t *c_TIMSK, const uint8_t c_OCIE, const uint8_t c_COM0, const int c_out_port, const int c_dir_port, const int c_en_port)
@@ -77,16 +82,19 @@ Stepper::Stepper(volatile uint16_t *c_OCR, volatile uint8_t *c_TCCRA, volatile u
 
     *TIMSK |= (1 << OCIE); // OCIEnX = Enable Compare Interrupts
                            // OCIEnX = Habilita Interrupções em Comparação
+                           
 
     direction = false;
     steps = -1;
+    moving = false;
     
     pinMode(out_port, OUTPUT);
     pinMode(dir_port, OUTPUT);
     pinMode(en_port, OUTPUT);
     
-    setSpeed(0);
-    setDirection(direction, -1);
+    setSteps(0, 0);
+    totalsteps = 0;
+    // setDirection(direction, -1);
     sei();
 };
 
@@ -97,26 +105,59 @@ void Stepper::setSpeed(short speed)
         *TCCRA &= ~(1 << COM0); // Disconnect OCnX from port
                                 // Disconecta a saída de OCnX do pino
 
-        digitalWrite(en_port, HIGH); // Disable motor by setting TB6560 EN+ to high
+        // digitalWrite(en_port, HIGH); // Disable motor by setting TB6560 EN+ to high
                                      // Desabilita motor configurando TB6560 EN+ para estado lógico alto
 
         digitalWrite(out_port, LOW);
+        moving = false;
+        return;
     } else
     {
         *TCCRA |= (1 << COM0);
-        digitalWrite(en_port, LOW);
+        moving = true;
+        // digitalWrite(en_port, LOW);
     }
-    int frequency = 8*speed + 200; 
+    int frequency = 8*speed + 50; 
     timer_reset_v = (1000000/frequency) - 1; // Calculate timer count for frequency based on ATMEGA2560 datasheet
                                              // Calcula contador para frequência baseado no datasheet do ATMEGA2560
 };
+
+void Stepper::setFrequency(uint16_t frequency)
+{
+  timer_reset_v = (1000000/frequency) - 1;
+  // Serial.println(timer_reset_v);
+}
+
+void Stepper::setSteps(bool direction, int c_steps)
+{
+    if (c_steps == 0) 
+    {
+        *TCCRA &= ~(1 << COM0); // Disconnect OCnX from port
+                                // Disconecta a saída de OCnX do pino
+
+        // digitalWrite(en_port, HIGH); // Disable motor by setting TB6560 EN+ to high
+                                     // Desabilita motor configurando TB6560 EN+ para estado lógico alto
+
+        digitalWrite(out_port, LOW);
+        moving = false;
+        return;
+    } else
+    {
+        *TCCRA |= (1 << COM0);
+        moving = true;
+    }
+
+    digitalWrite(dir_port, direction);
+    steps = c_steps;
+}
+
 
 // Configure direction and distance of movement based on axle of 1.419 cm
 // Configura direção e distância de movimento baseado em fuso com 1.419 cm
 void Stepper::setDirection(bool direction, int16_t distance)
 {
     digitalWrite(dir_port, direction);
-    steps = (distance == -1)?-1:(distance/1.419)*800;
+    steps = (distance == -1)?-1:(distance/1.8)*2;
 }
 
 // Allocate space for the motor array
@@ -132,7 +173,9 @@ ISR(TIMER1_COMPA_vect)
     *(motor[0].OCR) += motor[0].timer_reset_v; 
     if (motor[0].steps == 0)
     {
-        motor[0].setSpeed(0);
+        // motor[0].setDirection(!(motor[0].direction), motor[0].distance);
+        // motor[0].direction = !(motor[0].direction);
+        motor[0].setSteps(0, 0);
     } else if (motor[0].steps > -1)
     {
         motor[0].steps--;
@@ -144,7 +187,9 @@ ISR(TIMER3_COMPA_vect)
     *(motor[1].OCR) += motor[1].timer_reset_v; 
     if (motor[1].steps == 0)
     {
-        motor[1].setSpeed(0);
+        // motor[1].setDirection(!(motor[1].direction), motor[1].distance);
+        // motor[1].direction = !(motor[1].direction);
+        motor[1].setSteps(0, 0);
     } else if (motor[1].steps > -1)
     {
         motor[1].steps--;
@@ -156,7 +201,13 @@ ISR(TIMER4_COMPA_vect)
     *(motor[2].OCR) += motor[2].timer_reset_v;  
     if (motor[2].steps == 0)
     {
-        motor[2].setSpeed(0);
+        // Serial.println("Mudou");
+        // motor[2].setDirection(!(motor[2].direction), motor[2].distance);
+        // motor[2].direction = !(motor[2].direction);
+        
+        // motor[2].setSpeed(0);
+        motor[2].direction = !(motor[2].direction);
+        motor[2].setSteps(motor[2].direction, motor[2].totalsteps);
     } else if (motor[2].steps > -1)
     {
         motor[2].steps--;
@@ -168,7 +219,9 @@ ISR(TIMER1_COMPB_vect)
     *(motor[3].OCR) += motor[3].timer_reset_v;  
     if (motor[3].steps == 0)
     {
-        motor[3].setSpeed(0);
+        motor[3].setDirection(!(motor[3].direction), motor[3].distance);
+        motor[3].direction = !(motor[3].direction);
+        // motor[3].setSpeed(0);
     } else if (motor[3].steps > -1)
     {
         motor[3].steps--;
@@ -180,7 +233,9 @@ ISR(TIMER3_COMPB_vect)
     *(motor[4].OCR) += motor[4].timer_reset_v;  
     if (motor[4].steps == 0)
     {
-        motor[4].setSpeed(0);
+        motor[4].setDirection(!(motor[4].direction), motor[4].distance);
+        motor[4].direction = !(motor[4].direction);
+        // motor[4].setSpeed(0);
     } else if (motor[4].steps > -1)
     {
         motor[4].steps--;
@@ -192,7 +247,9 @@ ISR(TIMER4_COMPB_vect)
     *(motor[5].OCR) += motor[5].timer_reset_v;  
     if (motor[5].steps == 0)
     {
-        motor[5].setSpeed(0);
+        motor[5].setDirection(!(motor[5].direction), motor[5].distance);
+        motor[5].direction = !(motor[5].direction);
+        // motor[5].setSpeed(0);
     } else if (motor[5].steps > -1)
     {
         motor[5].steps--;
@@ -227,75 +284,39 @@ void setup() {
     motor[0] = Stepper(&OCR1A, &TCCR1A, &TCCR1B, &TIMSK1, OCIE1A, COM1A0, MOTOR_OUT_1, MOTOR_DIR_1, MOTOR_EN_1);
     motor[1] = Stepper(&OCR3A, &TCCR3A, &TCCR3B, &TIMSK3, OCIE3A, COM3A0, MOTOR_OUT_2, MOTOR_DIR_2, MOTOR_EN_2);
     motor[2] = Stepper(&OCR4A, &TCCR4A, &TCCR4B, &TIMSK4, OCIE4A, COM4A0, MOTOR_OUT_3, MOTOR_DIR_3, MOTOR_EN_3);
-    motor[3] = Stepper(&OCR1B, &TCCR1A, &TCCR1B, &TIMSK1, OCIE1B, COM1B0, MOTOR_OUT_4, MOTOR_DIR_4, MOTOR_EN_4);
-    motor[4] = Stepper(&OCR3B, &TCCR3A, &TCCR3B, &TIMSK3, OCIE3B, COM3B0, MOTOR_OUT_5, MOTOR_DIR_5, MOTOR_EN_5);
-    motor[5] = Stepper(&OCR4B, &TCCR4A, &TCCR4B, &TIMSK4, OCIE4B, COM4B0, MOTOR_OUT_6, MOTOR_DIR_6, MOTOR_EN_6);
+    // motor[3] = Stepper(&OCR1B, &TCCR1A, &TCCR1B, &TIMSK1, OCIE1B, COM1B0, MOTOR_OUT_4, MOTOR_DIR_4, MOTOR_EN_4);
+    // motor[4] = Stepper(&OCR3B, &TCCR3A, &TCCR3B, &TIMSK3, OCIE3B, COM3B0, MOTOR_OUT_5, MOTOR_DIR_5, MOTOR_EN_5);
+    // motor[5] = Stepper(&OCR4B, &TCCR4A, &TCCR4B, &TIMSK4, OCIE4B, COM4B0, MOTOR_OUT_6, MOTOR_DIR_6, MOTOR_EN_6);
+    motor[0].setFrequency(1600);
+    motor[1].setFrequency(1400);
+    motor[2].setFrequency(1200);
+    motor[2].totalsteps = 1300;
+    motor[2].setSteps(0, motor[2].totalsteps);
 }
 
 void loop() {
-    short motor_n;
-    short speed;
-    bool direction;
-    int16_t distance;
+    motor[0].setSteps(0, 4600);
+
+
+    while(motor[0].moving) {
+        delay(1);
+    }
     
-    #ifdef DEBUG
-        Serial.println("Motor 1: ");
-        Serial.print("TCCRA: ");
-        Serial.println(TCCR1A, BIN);
-        Serial.print("TCCRB: ");
-        Serial.println(TCCR1B, BIN);
-        Serial.print("TIMSK: ");
-        Serial.println(TIMSK1, BIN);
-        
-        Serial.println();
-
-        Serial.println("Motor 3: ");
-        Serial.print("TCCRA: ");
-        Serial.println(TCCR3A, BIN);
-        Serial.print("TCCRB: ");
-        Serial.println(TCCR3B, BIN);
-        Serial.print("TIMSK: ");
-        Serial.println(TIMSK3, BIN);
-
-        Serial.println();
-
-        Serial.println("Motor 4: ");
-        Serial.print("TCCRA: ");
-        Serial.println(TCCR4A, BIN);
-        Serial.print("TCCRB: ");
-        Serial.println(TCCR4B, BIN);
-        Serial.print("TIMSK: ");
-        Serial.println(TIMSK4, BIN);
-    #endif
-
-    do {
-        Serial.println("Qual motor voce gostaria de alterar? 1/2/3/4/5/6");
-        while (!Serial.available()){}
-        motor_n = Serial.readStringUntil('\n').toInt();
-    } while (motor_n < 1 || motor_n > 6);
-
-    motor_n--;
-
-    do {
-        Serial.println("Insira a velocidade: 0-100%");
-        while (!Serial.available()){}
-        speed = Serial.readStringUntil('\n').toInt();
-    } while (speed < 0 || speed > 100);
-
-    Serial.println("Insira a direcao: 0 - Horario/ 1 - Anti-horario");
-    while (!Serial.available()){}
-    direction = Serial.readStringUntil('\n').toInt();
-
-    do
-    {
-        Serial.println("Insira a distancia de movimento ou -1 para movimento continuo (mm):");
-        while (!Serial.available()){}
-        distance = Serial.readStringUntil('\n').toInt();
-    } while (distance != -1 && (distance < 0 || distance > 1000));
+    motor[1].setSteps(0, 2400);
     
-    Serial.print("Velocidade: ");
-    Serial.println(speed);
-    motor[motor_n].setSpeed(speed);
-    motor[motor_n].setDirection(direction, distance);
-    Serial.println();
+    while(motor[1].moving) {
+        delay(1);
+    }
+
+    motor[1].setSteps(1, 2400);
+
+    while(motor[1].moving) {
+        delay(1);
+    }
+
+    motor[0].setSteps(1, 4600);
+
+    while(motor[0].moving) {
+        delay(1);
+    }
 }
